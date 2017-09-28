44cb3bb9867dfc45d02a220e5f3b208656b9954e
mri-dev <pocokxtrame@gmail.com>
2017-09-28 19:48:37 +0200

    Save

--- wp-content/themes/Avada-Child-Theme/includes/shortcodes/shortcodes.php
+++ wp-content/themes/Avada-Child-Theme/includes/shortcodes/shortcodes.php
@@ -1,3 +1,4 @@
 <?php
   require_once "SVGDesignSC.php";
+  require_once "HomeMainDesign.php";
 ?>
---
+++ wp-content/themes/Avada-Child-Theme/templates/shortcodes/homemaindesign.php
@@ -0,0 +1,45 @@
+<div class="designholder">
+  <div class="page-width">
+    <div class="flxtbl ic3">
+      <div class="designbox tervez">
+        <div class="design-base">
+          <img src="<?=IMG?>/homdesign-tervez.svg" alt="">
+        </div>
+      </div>
+      <div class="designbox kivitelez">
+        <div class="cloud-des animate animate-right animate-slow cloud-dark cloud-big cld1"></div>
+        <div class="cloud-des animate animate-right cloud-white cloud-big cld2"></div>
+        <div class="cloud-des animate animate-right animate-slow cloud-dark cloud-big cld3"></div>
+        <div class="cloud-des animate animate-left cloud-white cloud-big cld4"></div>
+        <div class="cloud-des animate animate-left animate-slow cloud-dark cloud-big cld5"></div>
+        <div class="design-base">
+          <img src="<?=IMG?>/homdesign-kivitelez.svg" alt="">
+        </div>
+      </div>
+      <div class="designbox hosting">
+        <div class="cloud-des animate animate-right animate-slow cloud-dark cloud-big cld1"></div>
+        <div class="cloud-des animate animate-right cloud-white cloud-big cld2"></div>
+        <div class="cloud-des animate animate-left cloud-white cloud-sm cld3"></div>
+        <div class="cloud-des animate animate-left animate-slow cloud-dark cloud-sm cld4"></div>
+        <div class="design-base">
+          <img src="<?=IMG?>/homdesign-hosting.svg" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bottom-bar">
+  <div class="page-width">
+    <div class="flxtbl ic3">
+      <div class="tervez">
+        <a href="#">Egyedi weboldalak megtervezése</a>
+      </div>
+      <div class="kivitelez">
+        <a href="#">Weboldalak fejlesztése</a>
+      </div>
+      <div class="hosting">
+        <a href="#">Szuper biztonságos és gyors hosting</a>
+      </div>
+    </div>
+  </div>
+</div>
